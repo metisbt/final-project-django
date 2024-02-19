7d303ed54f7d5c69df406181515b29868325e055
Mohamad Mahdi Sabet <77541798+metisbt@users.noreply.github.com>
2024-02-19 04:15:19 +0330

    added contact and about page

--- website/templatetags/website_tags.py
+++ website/templatetags/website_tags.py
@@ -1,5 +1,5 @@
 from django import template
-from blog.models import Post
+from blog.models import Post, Comment
 from django.utils import timezone
 from datetime import datetime, timezone
 
@@ -9,3 +9,8 @@ register = template.Library()
 def latest_post_home():
     posts = Post.objects.filter(published_date__lte=datetime.now(tz=timezone.utc), status=1).order_by('-published_date')[:6]
     return {'posts' : posts}
+
+@register.inclusion_tag('website/latest-comment-home.html')
+def latest_comment_home():
+    comment = Comment.objects.filter(approved=True).order_by('-created_date')[:4]
+    return {'comment' : comment}
